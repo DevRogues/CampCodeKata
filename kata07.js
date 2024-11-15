edb5950d373d61dd7af262e611db93fa86ea9d33
EuiJoong <euijki87@gmail.com>
2024-11-15 12:10:52 +0900

    2
    2

--- kata07.js
+++ kata07.js
@@ -1,13 +1,4 @@
-const solution = (angle) =>
-  0 < angle && angle < 90
-    ? 1
-    : angle == 90
-    ? 2
-    : 90 < angle && angle < 180
-    ? 3
-    : angle === 180
-    ? 4
-    : null;
+const solution = (num1, num2) => Math.floor((num1 / num2) * 1000);
 
 // const solution = (angle) => 0 < angle < 90 ? 1 : angle == 90 ? 2 : 90 < angle < 180 ? 3 : angle === 180 ? 4 : null;
 
