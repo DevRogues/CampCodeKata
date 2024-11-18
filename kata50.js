f97bfb3af89ba7705ead7f6109f12b096c1f6cca
EuiJoong <euijki87@gmail.com>
2024-11-18 10:00:25 +0900

    Create kata50.js

---
+++ kata50.js
@@ -0,0 +1,26 @@
+let s = "banana";
+
+function solution(s) {
+  let answer = [];
+
+  for (let i = s.length - 1; i > 0; i--) {
+    let isIt = false;
+    for (let j = i - 1; j >= 0; j--) {
+      if (s[i] === s[j]) {
+        answer[i] = i - j;
+        isIt = true;
+        break;
+      } else {
+        isIt = false;
+      }
+    }
+
+    if (!isIt) {
+      answer[i] = -1;
+    }
+  }
+  answer[0] = -1;
+  return answer;
+}
+
+console.log(solution(s));
