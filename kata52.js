4c61396dad75fdd395691c44699b1d45bf35e579
EuiJoong <euijki87@gmail.com>
2024-11-20 15:44:52 +0900

    Update kata52.js

--- kata52.js
+++ kata52.js
@@ -4,6 +4,7 @@ let c = 20;
 
 function solution(a, b, c) {
   var answer = 0;
+
   while (true) {
     if (Math.floor(c / a) < 1) {
       answer += c % a;
@@ -18,3 +19,5 @@ function solution(a, b, c) {
 }
 
 console.log(solution(a, b, c));
+
+// solution = (a,b,n) => Math.floor(Math.max(n-b,0))
