39765f2f5f0a6cfdb701d22a84f5289b62b00027
EuiJoong <euijki87@gmail.com>
2024-11-20 10:01:53 +0900

    Create kata52.js

---
+++ kata52.js
@@ -0,0 +1,20 @@
+let a = 3;
+let b = 1;
+let c = 20;
+
+function solution(a, b, c) {
+  var answer = 0;
+  while (true) {
+    if (Math.floor(c / a) < 1) {
+      answer += c % a;
+      break;
+    }
+
+    answer += c % a === 0 ? Math.floor(c / a) : Math.floor(c / a) * b;
+    c = Math.floor(c / a);
+  }
+
+  return answer;
+}
+
+console.log(solution(a, b, c));
