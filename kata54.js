189bba84727e31b17eb9d4030083ac06bf65406e
EuiJoong <euijki87@gmail.com>
2024-11-22 09:30:56 +0900

    Update kata54.js

--- kata54.js
+++ kata54.js
@@ -1,6 +1,8 @@
-let s = [10, 100, 20, 150, 1, 100, 200];
-function solution(k, s) {
-  var answer = [];
-
-  return answer;
+function solution(a, b) {
+  var answer = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  return answer[new Date("2016-" + a + "-" + b).getDay()];
 }
+
+let a = 5;
+let b = 24;
+console.log(solution(a, b));
