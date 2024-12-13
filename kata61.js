2960314426bbb9c4a748e5485e672e37f55764b4
EuiJoong <euijki87@gmail.com>
2024-12-13 09:28:30 +0900

    2
    2

---
+++ kata61.js
@@ -0,0 +1,22 @@
+function solution(lottos, win_numsr) {
+  var zeroCnt = 0;
+  var aa = [31, 10, 45, 1, 6, 19];
+
+  for (let i = 0; i < lottos.length; i++) {
+    // lottos[i]
+    console.log(aa.find((lottos[i]) => \]\
+  
+  03
+.toExponential
+
+
+
+
+
+
+
+));
+  }
+}
+
+console.log(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]));
