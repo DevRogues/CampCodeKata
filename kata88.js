0cca7e8f9a7ea694e61e0ab0a3c0f55667edcfa7
EuiJoong <euijki87@gmail.com>
2024-12-26 10:10:28 +0900

    Update kata88.js

--- kata88.js
+++ kata88.js
@@ -6,11 +6,11 @@ function solution(arr1, arr2) {
   //arr1의 길이만큼 순회
   for (let i = 0; i < arr1.length; i++) {
     const row = [];
-    //arr2의 길이 만큼 순회
+    //arr2의 '열' 길이 만큼 순회
     for (let j = 0; j < arr2[0].length; j++) {
       let sum = 0;
 
-      //arr2의 길이 만큼 순회
+      //arr2의 '행'길이 만큼 순회
       for (let k = 0; k < arr2.length; k++) {
         sum += arr1[i][k] * arr2[k][j];
       }
