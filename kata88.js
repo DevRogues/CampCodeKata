dbddf8d9184ead67e6c37a499ebc2387e3cfdfae
EuiJoong <euijki87@gmail.com>
2024-12-26 10:09:28 +0900

    Create kata88.js

---
+++ kata88.js
@@ -0,0 +1,36 @@
+//행렬의 곱셈
+
+function solution(arr1, arr2) {
+  var answer = [];
+
+  //arr1의 길이만큼 순회
+  for (let i = 0; i < arr1.length; i++) {
+    const row = [];
+    //arr2의 길이 만큼 순회
+    for (let j = 0; j < arr2[0].length; j++) {
+      let sum = 0;
+
+      //arr2의 길이 만큼 순회
+      for (let k = 0; k < arr2.length; k++) {
+        sum += arr1[i][k] * arr2[k][j];
+      }
+      row.push(sum);
+    }
+    answer.push(row);
+  }
+
+  return answer;
+}
+console.log(
+  solution(
+    [
+      [1, 4],
+      [3, 2],
+      [4, 1],
+    ],
+    [
+      [3, 3],
+      [3, 3],
+    ]
+  )
+);
